dedc508585294c831147b0cb030d9caef4214b5f
VYR4L <fkz.zanatt@gmail.com>
2024-05-23 01:42:44 -0300

    Criado o sistema de links para mensagens

--- get_number.py
+++ get_number.py
@@ -6,10 +6,15 @@ from pathlib import Path
 import re
 import os
 
+
 ROOT_DIR = Path(__file__).parent
 IMAGES_DIR = ROOT_DIR / 'images'
 PROC_DIR = ROOT_DIR / 'processed_images'
 
+# TODO: Utilizar arquivos para salvar os números conforme o código for executado repetidademente
+numbers_list = []
+links_list = []
+link_prefix = "https://wa.me/"
 
 def process_images():
     for image in IMAGES_DIR.iterdir():
@@ -23,15 +28,22 @@ def process_images():
             processed_image_path = PROC_DIR / f'{no_extension_image}_processed.jpg'
             cv2.imwrite(str(processed_image_path), processed_image)
 
+
 def extract_number(numbers):
+    phone_pattern = re.compile(r'\b\d{2}\s\d{2}\s\d{4,5}-\d{4}\b')
     for image in PROC_DIR.iterdir():
         if image.suffix == '.jpg':
             image_read = Image.open(image)
             text = pts.image_to_string(image_read)
-            numbers.extend(re.findall(r'\d+', text))
+            numbers.extend(phone_pattern.findall(text))
 
-numbers_list = []
 
 process_images()
 extract_number(numbers_list)
-print(numbers_list)
+
+numbers_list_converted = [number.replace(" ", "").replace("-", "") for number in numbers_list]
+
+for i in numbers_list_converted:
+    links_list.append(link_prefix + i)
+
+print(links_list)
