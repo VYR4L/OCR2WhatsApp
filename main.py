29286a99c3c6da6697f7d06668baabc7782a0dfe
Felipe Kravec Zanatta <118224396+VYR4L@users.noreply.github.com>
2024-06-11 19:31:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ numbers_list = []
 numbers_list_converted = []
 excluded_numbers = []
 
-message = "Oi, prof! Tudo bem?! Eu sou a profa Marina, peguei seu número no grupo da DRE Metropolitana! Eu era de Cuiabá e atualmente estou no Paraná desenvolvendo minha pesquisa de doutorado com os docentes da rede estadual de MT! Você pode me ajudar com 5min do seu tempo respondendo ao formulário de pesquisa? É rapidinho, eu juro!! O link é do formulário Google:"
-link = "https://docs.google.com/forms/d/e/1FAIpQLSeCsytjpURRLgiEClRggardOoCiCeWbP-MBwyq5ELwMJKgWQA/viewform"
+message = "<your message>"
+link = "<your_link"
 extract_number(numbers_list, numbers_list_converted, excluded_numbers)
 send_whatsapp_message(numbers_list_converted, message, link)
